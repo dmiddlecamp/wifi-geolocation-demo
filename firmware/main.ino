46de39df799fed48b21a458e995de2b0a617274a
David Middlecamp <david@spark.io>
2016-09-23 10:28:34 -0500

    adding mozilla contribution hook

--- firmware/main.ino
+++ firmware/main.ino
@@ -1,15 +1,11 @@
-#define UPDATE_FREQUENCY 10000
+#define UPDATE_FREQUENCY 30000
 
 unsigned int lastPublish;
 
 void setup() {
     Serial.begin(115200);
   
-//  Particle.subscribe("hook-response/wifi_scan");
-  
-  //{\"location\":{\"lat\":44.9490002,\"lng\":-93.2920491},\"accuracy\":63.3364459}
-  //ook-response/wifi_scan/0","data":"{\"location\":{\"lat\":44.9490002,\"lng\":-93.2920491},\"accuracy\":63.3364459}","ttl":"60","published_at":"2016-09-22T22:05:38.800Z","coreid":"particle-internal"}
-
+    Particle.subscribe("hook-response/wifi_scan", onLocationReceived, MY_DEVICES);
 }
 
 void loop() {
@@ -52,7 +48,6 @@ void WifiScan() {
             count++;
         }
         
-        //{"name":"wifi_scan","data":"{\"list\":[{\"m\":\"70:5A:0F:F8:51:49\",\"s\":-60},{\"m\":\"6C:3B:E5:4E:7A:58\",\"s\":-77},{\"m\":\"52:6D:64:28:A2:C0\",\"s\":-70},{\"m\":\"B4:C7:99:B1:2F:F8\",\"s\":-65},{\"m\":\"B4:C7:99:B1:2F:F0\",\"s\":-66}]}","ttl":"60","published_at":"2016-09-22T21:56:48.130Z","coreid":"300040000a47343232363230"}
         
     }
     wifiList += "]}";
@@ -60,9 +55,52 @@ void WifiScan() {
     if (count == 5) {
         // for simplicity right now, lets only do it if we see at least 5 APs.
         Particle.publish("wifi_scan", wifiList, PRIVATE);
+        
+        // looks something like this
+        //{"name":"wifi_scan",
+        //"data":"{\"list\":[{\"m\":\"70:5A:0F:F8:51:49\",\"s\":-60},{\"m\":\"6C:3B:E5:4E:7A:58\",\"s\":-77},{\"m\":\"52:6D:64:28:A2:C0\",\"s\":-70},{\"m\":\"B4:C7:99:B1:2F:F8\",\"s\":-65},{\"m\":\"B4:C7:99:B1:2F:F0\",\"s\":-66}]}",
+        //"ttl":"60",
+        //"published_at":"2016-09-22T21:56:48.130Z",
+        //"coreid":"abc123"}
+    }
+}
+
+
+// data should be "lat,lng,accuracy"
+void onLocationReceived(const char *event, const char *data) {
+    if (strcmp(data, "{}") == 0) {
+        Serial.println("got a contribute link back");
+        return;
     }
     
+    Serial.println("got location back " + String(data));
+    
+    
+    char one[32], two[32], three[32];
+    float latitude, longitude, altitude;
+    //sscanf(someString, "%f,%f,%f", &latitude, &longitude, &altitude);
+    sscanf(data, "%[^,],%[^,],%[^,]", one, two, three);
+    
+    latitude = String(one).toFloat();
+    longitude = String(two).toFloat();
+    altitude = String(three).toFloat();
+    
+    if (latitude ==  0) {
+        Serial.println("nevermind");
+        return;
+    }
+    
+    
+    Serial.println(String::format("Parsed into %f, %f, %f ", latitude, longitude, altitude));
+    String dataJson = String("{")
+        + "\"c_lat\":" + String::format("%f", latitude)
+        + ",\"c_lng\":" + String::format("%f", longitude)
+        + ",\"c_unc\":" + String::format("%f", altitude)
+        + "}";
+        
+    Particle.publish("trk/loc", dataJson, 60, PRIVATE);
 }
+
 
 // For getting our location back
 //
@@ -96,28 +134,5 @@ void WifiScan() {
 //             }
 //         ]
 // }]}
-
-
-
-          
-
-// {
-//   "wifiAccessPoints": [
-//     {
-//       "macAddress": "{{list.0.m}}",
-//       "signalStrength": "{{list.0.s}}"
-//     },
-//     {
-//       "macAddress": "{{list.1.m}}",
-//       "signalStrength": "{{list.1.s}}"
-//     },
-//     {
-//       "macAddress": "{{list.2.m}}",
-//       "signalStrength": "{{list.2.s}}"
-//     }
-//   ]
-// }
-          
-
 
 
